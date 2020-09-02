42c50ea386ad23643cb90e557cf6b12c7f45d7c4
Tanngrisnr <fredrik.wintzell@gmail.com>
2020-09-02 20:43:26 +0200

    Added randomizing correct colors function

--- Color_game/colorGame.js
+++ Color_game/colorGame.js
@@ -8,9 +8,35 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = 
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.getElementById("message");
 
-for (var i = 0; i < squares.length; i++) {
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < squares.length; i++) {
+  //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
+  //add click listeners to squares
+  squares[i].addEventListener("click", function () {
+    //grab color of clicked square
+    let clickedColor = this.style.backgroundColor;
+    if (clickedColor === pickedColor) {
+      messageDisplay.textContent = "Correct";
+      changeColors(clickedColor);
+    } else {
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try again";
+    }
+  });
+}
+
+function changeColors(color) {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+function pickColor() {
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
